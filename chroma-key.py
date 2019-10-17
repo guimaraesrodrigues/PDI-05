4771662cdfd1d2963079c554536a4bdbc01af5ca
Nina <ninapallaro1@gmail.com>
2019-10-16 21:17:45 -0300

    adicionada mascara binaria

--- chroma-key.py
+++ chroma-key.py
@@ -3,13 +3,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 entrada = "img/0.bmp"
-input = cv.imread(entrada, cv.IMREAD_COLOR)
-
-imgHSV = cv.cvtColor(input, cv.COLOR_BGR2HSV)
-
-
+entrada2 = "img/5.bmp"
+objeto = cv.imread(entrada, cv.IMREAD_COLOR)
+fundo = cv.imread(entrada2, cv.IMREAD_COLOR)
 
 
-cv.imshow("input", imgHSV)
+
+imgHSV = cv.cvtColor(objeto, cv.COLOR_BGR2HSV)
+
+lower_green = np.array([45,50,50])
+upper_green= np.array([75,255,255])
+
+mask = cv.inRange(imgHSV, lower_green, upper_green)
+
+res = cv.bitwise_and(objeto,fundo, mask= mask)
+
+
+cv.imshow("mask", mask)
+cv.imshow("entrada", objeto)
 cv.waitKey(0)
 
