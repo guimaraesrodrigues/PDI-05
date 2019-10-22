208b5f8d6f5ff42ce03849a98216e7ab58ba7ccc
Nina <ninapallaro1@gmail.com>
2019-10-22 19:22:14 -0300

    nova mascara para regiao com conteudo desconhecido - nao sabemos se eh fundo ou objeto

--- chroma-key.py
+++ chroma-key.py
@@ -1,25 +1,34 @@
-import cv2 as cv
+import cv2 as cv2
 import numpy as np
+import math
 from matplotlib import pyplot as plt
 
-entrada = "img/0.bmp"
-entrada2 = "img/5.bmp"
-objeto = cv.imread(entrada, cv.IMREAD_COLOR)
-fundo = cv.imread(entrada2, cv.IMREAD_COLOR)
+# entrada = "img/0.bmp"
+# entrada = "img/1.bmp"
+# entrada = "img/2.bmp"
+# entrada = "img/3.bmp"
+# entrada = "img/4.bmp"
+# entrada = "img/5.bmp"
+# entrada = "img/6.bmp"
+# entrada = "img/7.bmp"
+# entrada = "img/8.bmp"
+objeto = cv2.imread(entrada, cv2.IMREAD_COLOR)
 
 
+imgHSV = cv2.cvtColor(objeto, cv2.COLOR_BGR2HSV)
 
-imgHSV = cv.cvtColor(objeto, cv.COLOR_BGR2HSV)
+lower_green = np.array([45,0,40])
+upper_green = np.array([75,255,255])
+upper_green2 = np.array([75,255,90])
 
-lower_green = np.array([45,50,50])
-upper_green= np.array([75,255,255])
+mask = cv2.inRange(imgHSV, lower_green, upper_green)
+mask = cv2.bitwise_not(mask)
 
-mask = cv.inRange(imgHSV, lower_green, upper_green)
+mask2 = cv2.inRange(imgHSV, lower_green, upper_green2)
+mask2 = cv2.bitwise_not(mask2)
 
-res = cv.bitwise_and(objeto,fundo, mask= mask)
-
-
-cv.imshow("mask", mask)
-cv.imshow("entrada", objeto)
-cv.waitKey(0)
+cv2.imshow("mask", mask)
+cv2.imshow("mask2", mask2)
+# cv2.imshow("entrada", objeto)
+cv2.waitKey(0)
 
