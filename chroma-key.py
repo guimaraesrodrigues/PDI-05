89f02a842b8a3bffb63c40c3a95796d13005f869
Nina <ninapallaro1@gmail.com>
2019-10-23 00:13:34 -0300

    algumas mudancas - torcer pra dar certo

--- chroma-key.py
+++ chroma-key.py
@@ -3,7 +3,9 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt
 
-TAM_JANELA = 36
+TAM_JANELA = 10
+SOMA_MAX = math.pow(TAM_JANELA * 2 + 1, 2) * 255
+
 
 #####
 # soma_max = TAM_JANELA * 255
@@ -11,8 +13,8 @@ TAM_JANELA = 36
 #####
 
 
-def is_out_of_bound(img, i, j):
-    return (i > img.shape[1] or i < 0) or (j > img.shape[0] or j < 0)
+def is_out_of_bound(img, i, j, tam):
+    return (i + tam> img.shape[1] or i - tam < 0) or (j + tam> img.shape[0] or j - tam < 0)
 
 
 def define_peso(img, img_fundo, img_objeto):
@@ -22,32 +24,27 @@ def define_peso(img, img_fundo, img_objeto):
 
     for linha in range(img.shape[0]):
         for coluna in range(img.shape[1]):
-
-            if img[linha][coluna] == 255:
-                peso = 1
+            print ("linha: ", linha, " coluna: ", coluna)
+            if img[linha][coluna] == 0:
+                img_final[linha][coluna] = img_fundo[linha][coluna]
 
             if img[linha][coluna] == 128:
                 soma_janela = 0
 
-                for linha_janela in range(linha - TAM_JANELA, linha + TAM_JANELA):
-                    for coluna_janela in range(coluna - TAM_JANELA, coluna + TAM_JANELA):
-
-                        if not is_out_of_bound(img, linha_janela, coluna_janela):
+                if not is_out_of_bound(img, linha, coluna, TAM_JANELA):
+                    for linha_janela in range(linha - TAM_JANELA, linha + TAM_JANELA):
+                        for coluna_janela in range(coluna - TAM_JANELA, coluna + TAM_JANELA):
                             soma_janela = soma_janela + img[linha_janela][coluna_janela]
 
-                soma_max = math.pow(TAM_JANELA * 2 + 1, 2) * 255
-                peso = soma_janela / soma_max
+                    peso = soma_janela / SOMA_MAX
+                    for canal in range(3):
+                        img_final[linha][coluna][canal] = math.floor(img_objeto[linha][coluna][canal] * peso + img_fundo[linha][coluna][canal] * (1 - peso))
 
-
-            for canal in range(3):
-                img_final[linha][coluna][canal] = math.floor(img_objeto[linha][coluna][canal] * peso + img_fundo[linha][coluna][canal] * (1 - peso))
-
-    cv2.imshow("entrada", img_final)
-    cv2.waitKey(0)
+    cv2.imshow("Final", img_final)
+    cv2.imwrite("5_Final.bmp", img_final)
 
 
 
-                # print("pixel ", img[linha][coluna])
 
 entrada = "img/0.bmp"
 # entrada = "img/1.bmp"
@@ -80,8 +77,14 @@ final_mask = cv2.addWeighted(mask, 1, mask2, 0.5, 0)
 define_peso(final_mask, novo_fundo, objeto)
 
 cv2.imshow("final_mask", final_mask)
-# cv2.imshow("mask2", mask2)
+cv2.imshow("mask2", mask2)
+cv2.imshow("mask", mask)
 cv2.imshow("entrada", objeto)
+
+cv2.imwrite("res/4_final_mask.bmp", final_mask)
+cv2.imwrite("res/3_mask2.bmp", mask2)
+cv2.imwrite("res/2_mask.bmp", mask)
+cv2.imwrite("res/1_entrada.bmp", objeto)
 cv2.waitKey(0)
 
 
